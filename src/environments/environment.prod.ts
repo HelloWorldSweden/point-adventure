2f6b98ec7eb61a9a2b8e30eafa25fd51cc8b1732
Johan Myrsmeden <johan@myrsmeden.se>
2017-07-26 22:13:55 +0200

    New production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDiUOeWJkpo63Zisz6OcXh-Paof7li8d34',
+    authDomain: 'hw-point-adventure.firebaseapp.com',
+    databaseURL: 'https://hw-point-adventure.firebaseio.com',
+    projectId: 'hw-point-adventure',
+    storageBucket: 'hw-point-adventure.appspot.com',
+    messagingSenderId: '685750042844'
+  }
 };
